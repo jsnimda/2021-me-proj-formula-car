7a8965be50e93de6fb7abb6c8af0ae2ad34f0e66
jsnimda <7615255+jsnimda@users.noreply.github.com>
2021-05-10 15:01:56 +0800

    Update CircularBuffer

--- platformio0508-test/lib/MyLibrary/CircularBuffer.h
+++ platformio0508-test/lib/MyLibrary/CircularBuffer.h
@@ -6,7 +6,12 @@
 
 #include "Print.h"
 
-template <size_t BUF_SIZE>
+enum BufferOverflowAction {
+  BUFFER_OVERWRITE,
+  WRITE_DISCARD,
+};
+
+template <size_t BUF_SIZE, BufferOverflowAction OVERFLOW_ACTION = BUFFER_OVERWRITE>
 class CircularBuffer_T : public Print {
   enum { cap = BUF_SIZE };  // unnamed enum for constant without allocating memory
 
@@ -16,6 +21,7 @@ class CircularBuffer_T : public Print {
   size_t _start = 0;    // inclusive index
   size_t _end = 0;      // exclusive index
   size_t overflow = 0;  // buffer overflowed count, be sure to clear this after checking
+  size_t highMark = 0;  // high water mark, debug only
 
   CircularBuffer_T(){};
 
@@ -35,8 +41,17 @@ class CircularBuffer_T : public Print {
   size_t write(const uint8_t *buffer, size_t size) override {
     size_t rem = remaining();
     if (size > rem) {
-      overflow += rem - size;
-      size = rem;
+      overflow += size - rem;
+      if (OVERFLOW_ACTION == WRITE_DISCARD) {
+        size = rem;
+      } else {  // BUFFER_OVERWRITE or default
+        if (size > cap) {
+          buffer += size - cap;  // only save last (size) bytes to buffer
+          size = cap;
+        }
+        // size >= rem here (size == rem if size > cap && rem == cap)
+        drop(size - rem);  // give up the space in buffer
+      }
     }
 
     if (size == 0) return 0;
@@ -47,7 +62,34 @@ class CircularBuffer_T : public Print {
     memcpy(_buf, buffer + a, size - a);
     _len += size;
     _end = _start + _len;
-    if (_end >= cap) _end -= cap;
+    if (_end >= cap) {
+      _end -= cap;
+      highMark = max(highMark, cap);
+    } else {
+      highMark = max(highMark, _end);
+    }
+
+    return size;
+  }
+
+  inline __attribute__((always_inline)) void _drop_no_check(size_t size) {
+    _len -= size;
+    if (_len == 0) {  // reset to 0
+      _start = 0;
+      _end = 0;
+    } else {
+      _start += size;
+      if (_start >= cap) _start -= cap;
+    }
+  }
+  size_t drop(size_t size) {
+    if (size > _len) {
+      size = _len;
+    }
+
+    if (size == 0) return 0;
+
+    _drop_no_check(size);
 
     return size;
   }
@@ -66,9 +108,7 @@ class CircularBuffer_T : public Print {
     size_t a = size > cap - _start ? cap - _start : size;
     memcpy(buffer, _buf + _start, a);
     memcpy(buffer + a, _buf, size - a);
-    _len -= size;
-    _start += size;
-    if (_start >= cap) _start -= cap;
+    _drop_no_check(size);
 
     return size;
   }
