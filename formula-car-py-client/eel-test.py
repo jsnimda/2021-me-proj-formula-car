6e08d2e2337e55d7cacb4a74cd2d035f1cb962b2
jsnimda <7615255+jsnimda@users.noreply.github.com>
2021-05-18 00:31:07 +0800

    Add eel client demo

--- formula-car-py-client/eel-test.py
+++ formula-car-py-client/eel-test.py
@@ -1,38 +1,30 @@
-import os
-import platform
-import sys
-
-# Use latest version of Eel from parent directory
-sys.path.insert(1, '../../')
 import eel
-
-# Use the same static files as the original Example
-os.chdir(os.path.join('..', '01 - hello_world'))
-
-# Set web files folder and optionally specify which file types to check for eel.expose()
-eel.init('web', allowed_extensions=['.js', '.html'])
+eel.init('web')
 
 
-@eel.expose                         # Expose this function to Javascript
-def say_hello_py(x):
-    print('Hello from %s' % x)
+@eel.expose
+def send_text(s):
+    print(s)
+    eel.set_hist(s)
 
 
-say_hello_py('Python World!')
-eel.say_hello_js('Python World!')   # Call a Javascript function
+eel.start('client.html', size=(1600, 900), block=False)
 
-# Launch example in Microsoft Edge only on Windows 10 and above
-if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
-    eel.start('hello.html', mode='edge')
-else:
-    raise EnvironmentError('Error: System is not Windows 10 or above')
 
-# # Launching Edge can also be gracefully handled as a fall back
-# try:
-#     eel.start('hello.html', mode='chrome-app', size=(300, 200))
-# except EnvironmentError:
-#     # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
-#     if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
-#         eel.start('hello.html', mode='edge')
-#     else:
-#         raise
+print(eel.js_random()())
+
+def my_other_thread():
+    while True:
+        print("I'm a thread")
+        eel.sleep(1.0)                  # Use eel.sleep(), not time.sleep()
+
+# eel.spawn(my_other_thread)
+
+eel.set_hist({
+    'test': [1,2,3,4],
+    'nono': 456
+})
+
+while True:
+    # print("I'm a main loop")
+    eel.sleep(1.0)   
